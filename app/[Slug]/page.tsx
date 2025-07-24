6dcd167bc942b5c4c03cabb9a93cc9058b2cde4b
controlscoder-rtfm <controlscoder@gmail.com>
2025-07-23 21:58:44 -0500

    Create page.tsx

---
+++ app/[Slug]/page.tsx
@@ -0,0 +1,19 @@
+import { allPages } from 'contentlayer/generated';
+import { notFound } from 'next/navigation';
+import { useMDXComponent } from 'next-contentlayer/hooks';
+
+export async function generateStaticParams() {
+  return allPages.map((page) => ({ slug: page.slug.slice(1) }));
+}
+
+export default function Page({ params }) {
+  const page = allPages.find((p) => p.slug === `/${params.slug}`);
+  if (!page) return notFound();
+  const MDXContent = useMDXComponent(page.body.code);
+  return (
+    <article>
+      <h1>{page.title}</h1>
+      <MDXContent />
+    </article>
+  );
+}
